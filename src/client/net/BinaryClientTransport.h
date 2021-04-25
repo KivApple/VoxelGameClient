26dc055f6bcf3e2b6755e9a6169982a5cd34c748
Ivan Kolesnikov <kiv.apple@gmail.com>
2021-04-25 23:33:51 +0200

    Added deserialization helper to BinaryClientTransport

--- src/client/net/BinaryClientTransport.h
+++ src/client/net/BinaryClientTransport.h
@@ -18,13 +18,23 @@ class BinaryClientTransport: public ClientTransport {
 	void sendPlayerPosition();
 
 protected:
+	template<typename T> static void deserialize(const std::string &data, T& message) {
+		bitsery::quickDeserialization<bitsery::InputBufferAdapter<std::string>>(
+				{data.begin(), data.size()},
+				message
+		);
+	}
+	
 	void handleMessage(const void *data, size_t dataSize);
+
 	template<typename T> void serializeAndSendMessage(const T &message) {
 		std::vector<uint8_t> buffer;
 		auto count = bitsery::quickSerialization<bitsery::OutputBufferAdapter<std::vector<uint8_t>>>(buffer, message);
 		sendMessage(buffer.data(), count);
 	}
+
 	virtual void sendMessage(const void *data, size_t dataSize) = 0;
+
 	void sendHello();
 	
 public:
