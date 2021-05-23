fcd7f79bf7ea1ea0afb0573235b5c9444253c74b
Ivan Kolesnikov <kiv.apple@gmail.com>
2021-05-23 17:37:35 +0200

    Restore glass in the inventory

--- src/server/net/ClientConnection.cpp
+++ src/server/net/ClientConnection.cpp
@@ -18,9 +18,9 @@ ClientConnection::ClientConnection(ServerTransport &transport): m_transport(tran
 	m_inventory[0].setType(transport.engine()->voxelTypeRegistry().get("grass"));
 	m_inventory[1].setType(transport.engine()->voxelTypeRegistry().get("dirt"));
 	m_inventory[2].setType(transport.engine()->voxelTypeRegistry().get("stone"));
-	m_inventory[3].setType(transport.engine()->voxelTypeRegistry().get("lava"));
-	m_inventory[4].setType(transport.engine()->voxelTypeRegistry().get("glass"));
-	m_inventory[4].setType(transport.engine()->voxelTypeRegistry().get("water"));
+	m_inventory[3].setType(transport.engine()->voxelTypeRegistry().get("glass"));
+	m_inventory[4].setType(transport.engine()->voxelTypeRegistry().get("lava"));
+	m_inventory[5].setType(transport.engine()->voxelTypeRegistry().get("water"));
 }
 
 void ClientConnection::updatePosition(const glm::vec3 &position, float yaw, float pitch, int viewRadius) {
